8208579f16559b03b87b9dacc94edacd5f054c6c
jose-luis-nunez <josenunez@outlook.de>
2022-08-07 02:09:28 +0200

    update second example

--- src/test/kotlin/example2/model2/CustomerContainer.kt
+++ src/test/kotlin/example2/model2/CustomerContainer.kt
@@ -1,7 +1,7 @@
 package example2.model2
 
 data class CustomerContainer(
-    override var name: String,
+    override var name: String = "",
     override var refund: Long = 0
 ) : Customer(
     name,
